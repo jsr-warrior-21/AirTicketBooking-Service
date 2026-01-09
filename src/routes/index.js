9ab8d91bab1fbbf41853798837c8db445f151243
Arvind <jsrarvind12@gmail.com>
2026-01-10 00:26:46 +0530

    migration of db

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-
+const v1ApiRoutes= require('./v1/index');
 const express = require("express");
 const router = express.Router();
 
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,7 +1,3 @@
 const express = require("express");
-const v1ApiRoutes= require('./v1/index');
 const router = express.Router();
-
-router.use('/v1',v1ApiRoutes);
-
 module.exports =  router;
