b9ca32d07aacd6867c6d376d0350bd02eb54cee0
Arvind <jsrarvind12@gmail.com>
2026-01-09 23:53:33 +0530

    setting up skelton of Booking service

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const { PORT } = require("./config/serverConfig");
+const apiRoutes = require("./routes/index");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const db = require("./models/index");
+
+const setupAndStartServer = () => {
+  app.use(bodyParser.json()); // both line help us to read the req body containt properly
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use("/api", apiRoutes);
+
+  app.listen(PORT, () => {
+    console.log(`server started at PORT : ${PORT}`);
+
+    if (process.env.DB_SYNC) {
+      db.sequelize.sync({ alter: true });
+    }
+  });
+};
+setupAndStartServer();
