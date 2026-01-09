c80df3178e420e3f52a58ddf2f880df37cd2a960
Arvind <jsrarvind12@gmail.com>
2026-01-10 00:13:51 +0530

    setting up error file

---
+++ src/utils/error/app-error.js
@@ -0,0 +1,15 @@
+
+
+class AppError extends Error{
+    constructor(
+        name,message,explanation,statusCode
+    ){
+        super();
+        this.name = name,
+        this.explanation = explanation,
+        this.message = message,
+        this.statusCode = statusCode
+    }
+}
+
+module.exports = AppError;
